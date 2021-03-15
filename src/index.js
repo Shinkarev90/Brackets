e36109827d2ddb9185e6dd15b509852bf9dd4ad9
shinkare90 <55100900+Shinkarev90@users.noreply.github.com>
2021-03-16 00:51:34 +0400

    feat: add Brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let arr = str;
+  while (/\{\}|\(\)|\|\||\[\]|12|34|56|77|88/.test(arr)) {
+    arr = arr.replace(/\{\}|\(\)|\|\||\[\]|12|34|56|77|88/ig, '');
+  }
+  if (arr.length == 0){
+    return true;
+  } else if (arr.length > 0) {
+    return false;
+  }
+};
